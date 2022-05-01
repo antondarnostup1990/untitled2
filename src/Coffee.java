176bf08af3f31c1b89696f2e220a34053640043a
Anton.Darnostup <antondarnostup1990@gmail.com>
2022-05-01 19:48:59 +0300

    5

--- src/Coffee.java
+++ src/Coffee.java
@@ -4,4 +4,10 @@ public int b;
 public String c;
 public int tea;
 
+    public Coffee(Integer a, int b, String c, int tea) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.tea = tea;
+    }
 }
